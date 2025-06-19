b667f3ef4ca67e550b14f4ce254285d4732990f9
Arfiannn <arfianpndg@gmail.com>
2025-06-20 02:15:41 +0800

    Menambahkan model Checkout dengan relasi ke User, Product, Keranjang, dan Alamat menggunakan GORM.

---
+++ models/checkout.go
@@ -0,0 +1,22 @@
+package models
+
+type Checkout struct {
+	IDCheckout       uint   `gorm:"column:id_checkout;primaryKey;autoIncrement" json:"id_checkout"`
+	IDUser           uint   `gorm:"column:id_user" json:"id_user"`
+	IDProduct        uint   `gorm:"column:id_product" json:"id_product"`
+	IDKeranjang      *uint  `gorm:"column:id_keranjang" json:"id_keranjang"` // nullable FK
+	IDAlamat         uint   `gorm:"column:id_alamat" json:"id_alamat"`
+	MetodePengiriman string `gorm:"column:metode_pengiriman" json:"metode_pengiriman"`
+	Pembayaran       string `gorm:"column:pembayaran" json:"pembayaran"`
+	Jumlah           int    `gorm:"column:jumlah" json:"jumlah"`
+	SizeP            string `gorm:"column:sizeP" json:"sizeP"`
+
+	User      User       `gorm:"foreignKey:IDUser;references:IDUser" json:"user"`
+	Product   Product    `gorm:"foreignKey:IDProduct;references:IDProduct" json:"product"`
+	Keranjang *Keranjang `gorm:"foreignKey:IDKeranjang;references:IDKeranjang" json:"keranjang,omitempty"`
+	Alamat    Alamat     `gorm:"foreignKey:IDAlamat;references:IDAlamat" json:"alamat"`
+}
+
+func (Checkout) TableName() string {
+	return "checkout"
+}
