fe5954a8a676704d829ad773f52811e91519744f
rioadityapratama <rioaditya444@gmail.com>
2025-06-20 02:03:41 +0800

    - Membuat struct History berisi id_product, id_user, jumlah, dan sizeH - Menambahkan relasi ke model Product dan User

---
+++ models/history.go
@@ -0,0 +1,12 @@
+package models
+
+type History struct {
+	IDHistory uint   `gorm:"column:id_history;primaryKey;autoIncrement" json:"id_history"`
+	IDProduct uint   `gorm:"column:id_product" json:"id_product"`
+	IDUser    uint   `gorm:"column:id_user" json:"id_user"`
+	Jumlah    int    `gorm:"column:jumlah" json:"jumlah"`
+	SizeH     string `gorm:"column:sizeH" json:"sizeH"`
+
+	Product Product `gorm:"foreignKey:IDProduct;references:IDProduct" json:"product"`
+	User    User    `gorm:"foreignKey:IDUser;references:IDUser" json:"user"`
+}
